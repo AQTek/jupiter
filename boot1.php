9b28549652ee18cc06716e1eccedbba25e7ac5b2
unknown <ngththtoan@gmail.com>
2023-04-20 11:05:28 +0700

    20 Apr
    Aaa

---
+++ boot1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href ="css/bootstrap.min.css">
+       <script src="js/bootstrap.bundle.min.js" ></script>
+       
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    
+    <div class="container text-center">
+        <div class ="row">
+            <div class = "col-md-8" >
+                <div class = "row">
+                    <div class ="col-md-6">
+
+                            <button type="button" class="btn btn-primary">Học viện Công nghệ Massachusetts được viết tắt là M.I.T (Massachusetts Institute of Technology)</button>
+                        
+                    </div>
+                    
+                    <div class = "col-md-6">
+                        <button type="button" class="btn btn-secondary">Học viện Công nghệ Massachusetts </button>
+                        
+                    
+                    </div>
+                </div>
+            </div>
+          
+            <div class = "col-md-4" >
+            <button type="button" class="btn btn-warning">Học viện Công nghệ Massachusetts được viết tắt là M.I.T (Massachusetts Institute of Technology),</button>
+            </div>
+        </div>
+        
+    </div>
+    <hr>
+    <div class="container text-center">
+        <ul>
+            <li ><a href="http://cisco.com" > Cisco </a> </li>
+            <li ><a href="http://hp.com" target="_blank" > HP </a> </li>
+            <li ><a href="#" > Dell </a> </li>
+            <li ><a href="#" > Netapp </a> </li>
+        </ul>
+
+    </div>
+    <div class = "container text-center">
+        <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-primary">BUY</button>
+            <button type="button" class="btn btn-secondary">SELL</button>
+            <button type="button" class="btn btn-warning">MAK/MTL</button>
+        </div>
+    </div>
+
+    <br>
+    <br>
+   
+  </body>
+</html>
